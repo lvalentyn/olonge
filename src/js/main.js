26a0cc3fff0fd4549851406787dd5302aa9e683a
LukashevV <valikallstars@gmail.com>
2021-07-07 15:19:15 +0300

    desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,6 @@
 window.onload = function () {
-    document.addEventListener('click', documentActions);
-    function documentActions(e) {
-        const targetElement = e.target;
-        if (window.innerWidth > 768) {
-            if (targetElement.classList.contains('menu__arrow')) {
-                targetElement.closest('.menu__item').classList.toggle('_hover');
-            }
-            if (!targetElement.closest('.menu__item') && document.querySelectorAll('.menu__item._hover').length > 0) {
-                _removeClasses(document.querySelectorAll('.menu__item._hover'), '_hover');
-            }
-        }
-    }
+    // paralax
+    $(".tels__img_left").paroller({ factor: -0.02, type: 'foreground', direction: 'vertical', transition: 'transform 0.2s ease-in' });
+    $(".tels__img_right").paroller({ factor: 0.02, type: 'foreground', direction: 'vertical', transition: 'transform 0.2s ease-in' });
+
 }
